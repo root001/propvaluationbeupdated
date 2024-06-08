1bfd15a79616814df52ba8a9c38d96c9f1b0cd9c
Abdul-Basit <abdul-basit.adebayo@ng.airtel.com>
2024-06-08 22:34:12 +0100

    borrower

--- src/main/java/com/mcb/abdulbasit/valuation/service/BorrowerService.java
+++ src/main/java/com/mcb/abdulbasit/valuation/service/BorrowerService.java
@@ -2,12 +2,16 @@ package com.mcb.abdulbasit.valuation.service;
 
 import ch.qos.logback.core.pattern.color.BoldWhiteCompositeConverter;
 import com.mcb.abdulbasit.valuation.model.Borrower;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BorrowerService {
 
     public Borrower createBorrower(Borrower borrower){
+        if(ObjectUtils.isEmpty(borrower) )
+            throw new UsernameNotFoundException("no user");
         return borrower;
     }
 }
